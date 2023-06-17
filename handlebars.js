d6feef92e1d43253ca34cf9c29b057af8843044e
jaroslawkubiak <jaroslawkubiak82@gmail.com>
2023-06-17 19:34:05 +0200

    finishing hero section + slider funct

--- handlebars.js
+++ handlebars.js
@@ -1,17 +1,95 @@
 (function fill() {
-  console.log("funkcja fill");
   const data = {
     navMenu: [
-        {link: "o-mnie", name: "o mnie"},
-        {link: "#", name: "oferta"},
-        {link: "#", name: "współpraca"},
-        {link: "#", name: "kontakt"},
+      { link: "o-mnie", name: "o mnie" },
+      { link: "#", name: "oferta" },
+      { link: "#", name: "współpraca" },
+      { link: "#", name: "kontakt" },
     ],
-    title: "to jest naglowek",
-    nav: "nawigacja"
+    slidebox: [
+      {
+        title: "Reprezentuję przedsiębiorców",
+        text: "w czynnościach z zakresu <strong>postępowań podatkowych, kontroli podatkowej, postępowań przed sądami administracyjnymi.</strong>",
+      },
+      {
+        title: "Będąc ekonomistą, prawnikiem",
+        text: "i prezesem firmy podatkowo-księgowej patrzę na problemy biznesu z merytoryczną wnikliwością, opierając się na wiedzy z wielu dziedzin i na praktycznym doświadczeniu.",
+      },
+    ],
   };
   const template = Handlebars.compile(document.getElementById("template").innerHTML);
   const filled = template(data);
   document.getElementById("output").innerHTML = filled;
 })();
 
+
+/////////////////////////////////////////////////////
+//slider effect
+const slides = document.querySelectorAll('.slide');
+const dotContainer = document.querySelector('.dots');
+let curSlide = 0;
+const maxSlide = slides.length;
+// creating "dots"
+const createDots = function () {
+  slides.forEach(function (_, i) {
+    dotContainer.insertAdjacentHTML(
+      'beforeend',
+      `<span data-slide="${i}" class="dots__dot cursor-pointer block w-16 h-4 rounded-full mb-2 border-dotBorder border hover:bg-dotActive">&nbsp;</span>`
+    );
+  });
+};
+
+//change slide
+const changeSlide = function () {
+  curSlide === maxSlide - 1 ? (curSlide = 0) : curSlide++;
+  goToSlide(curSlide);
+  activeDot(curSlide);
+};
+
+// changing slide after 5s
+setInterval(changeSlide, 5000);
+
+// selecting active dot
+const activeDot = function (slide) {
+  document
+    .querySelectorAll('.dots__dot')
+    .forEach(dot => dot.classList.remove('bg-dotActive'));
+  document
+    .querySelector(`.dots__dot[data-slide="${slide}"]`)
+    .classList.add('bg-dotActive');
+};
+
+// changing slides
+const goToSlide = function (slide) {
+  slides.forEach(
+    (s, i) => (s.style.transform = `translateX(${120 * (i - slide)}%)`)
+  );
+};
+// init for dots
+const init = function () {
+  createDots();
+  goToSlide(0);
+  activeDot(0);
+};
+init();
+
+// event listener for dots
+dotContainer.addEventListener('click', function (e) {
+  if (e.target.classList.contains('dots__dot')) {
+    const { slide } = e.target.dataset;
+    goToSlide(slide);
+    activeDot(slide);
+  }
+});
+
+
+
+// handling underline on menu links
+const nav = document.getElementById("nav-menu");
+const hendleHover = function (e) {
+  if (e.target.classList.contains("nav-link")) {
+    const link = e.target.children[0].classList.toggle("hidden");
+  }
+};
+nav.addEventListener("mouseover", hendleHover);
+nav.addEventListener("mouseout", hendleHover);
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,21 @@ module.exports = {
       colors: {
         primary: "#002e72",
         dotActive: "#d13100",
+        dotNotActive: "#fff",
+        dotBorder: "#bfbfbf",
         industries: "#f0f0f0",
-      }
+
+      },
+      width: {
+        1140: "1140px",
+        1200: "1200px",
+        456: "456px",
+      },
+      height: {
+        760: "760px",
+      },
+
     },
   },
   plugins: [],
-}
-
+};
